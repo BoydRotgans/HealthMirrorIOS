49fb15295c281c24f91288d91768a87dd33e7e24
BoydRotgans <boyd@rndr.studio>
2020-01-23 13:08:04 +0100

    make buttons and uitypeview

--- ControllVideos/ItemCell/ItemCell.swift
+++ ControllVideos/ItemCell/ItemCell.swift
@@ -10,17 +10,30 @@ import UIKit
 
 class ItemCell: UICollectionViewCell {
     
-    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var userButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     func setData(text: String) {
-        self.textLabel.text = text
+        self.userButton.setTitle(text, for:.normal)
+        
+        userButton.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
+        
     }
     
-    
+    @objc func buttonClick() {
+        print("set user \(self.userButton.titleLabel?.text as! String)")
+        
+        // save to userDefaults
+        
+        UserDefaults.standard.set(self.userButton.titleLabel?.text!, forKey: "selectedUser")
+        
+        // open next storyboard
+        //let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        //let newViewController = storyBoard.instantiateViewController(withIdentifier: "TypeViewController") as! UiTypeView
+        //self.present(newViewController, animated: true, completion: nil)
+    }
 
 }
---
+++ ControllVideos/TypeView/UiTypeView.swift
@@ -0,0 +1,30 @@
+//
+//  UiTypeView.swift
+//  ControllVideos
+//
+//  Created by Boyd Rotgans on 23/01/2020.
+//  Copyright © 2020 Ferdinand Sorg. All rights reserved.
+//
+
+import UIKit
+
+class UiTypeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
