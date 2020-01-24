c522a3bd40a30d3cf61697b5ac1640218ae42ec0
Ferdinand Sorg <post@ferdinands.org>
2020-01-24 16:16:55 +0100

    share button on user board

--- ControllVideos/UserBoardView/UserBoardViewController.swift
+++ ControllVideos/UserBoardView/UserBoardViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import Foundation
 import CSV
+import MessageUI
 
 class UserBoardViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var addUserButton: UIButton!
     
+    @IBOutlet weak var shareButton: UIButton!
     
     var dataArray = [] as [String]
     
@@ -24,6 +26,8 @@ class UserBoardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        shareButton.addTarget(self, action: #selector(sendEmail), for: .touchUpInside)
+        
         // set delegates
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
@@ -183,4 +187,65 @@ extension UserBoardViewController: UICollectionViewDelegateFlowLayout {
     }
     
     
+}
+
+extension UserBoardViewController: MFMailComposeViewControllerDelegate {
+    // Send Mail
+    
+    
+    
+    @objc func sendEmail() {
+        
+        print("hi im mailing")
+
+
+
+        let path = getDocumentsDirectory()
+        let trackingData = path.appendingPathComponent("trackingData.csv").path
+
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.setSubject("CSV File")
+            mailComposer.setMessageBody("Here is the CSV File.", isHTML: false)
+            mailComposer.setToRecipients(["post@ferdinands.org"])
+
+            let url = URL(fileURLWithPath: trackingData)
+
+            do {
+            let attachmentData = try Data(contentsOf: url)
+                mailComposer.addAttachmentData(attachmentData, mimeType: "text/csv", fileName: "trackingData")
+                mailComposer.mailComposeDelegate = self
+                self.present(mailComposer, animated: true
+                    , completion: nil)
+            } catch let error {
+                print("We have encountered error \(error.localizedDescription)")
+            }
+
+        } else {
+            print("Email is not configured in settings app or we are not able to send an email")
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .cancelled:
+            print("User cancelled")
+            break
+
+        case .saved:
+            print("Mail is saved by user")
+            break
+
+        case .sent:
+            print("Mail is sent successfully")
+            break
+
+        case .failed:
+            print("Sending mail is failed")
+            break
+        default:
+            break
+        }
+        controller.dismiss(animated: true)
+    }
 }
